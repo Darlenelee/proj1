6a1b809f78a1de92c791f1bfde834777f91a791c
Darlenelee <1215664347@qq.com>
2017-11-17 21:08:43 +0800

    a primary component of input todos

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
+  todos: Todo[] = [];
+  desc = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTodo() {
+    this.todos.push({
+      id: 1,
+      desc: this.desc,
+      completed: false
+    });
+    this.desc = '';
+  }
 }
