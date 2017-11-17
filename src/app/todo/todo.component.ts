85d3ae960709dfa9394de3e9e54d79670b9ea4b1
Darlenelee <1215664347@qq.com>
2017-11-17 21:39:12 +0800

    use uuid to create random id string

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
   todos: Todo[] = [];
-  desc = '';
-  constructor() { }
+  desc: string = '';
+  constructor(private service:TodoService) { }
 
   ngOnInit() {
   }
-
-  addTodo() {
-    this.todos.push({
-      id: 1,
-      desc: this.desc,
-      completed: false
-    });
+  addTodo(){
+    this.todos = this.service.addTodo(this.desc);
     this.desc = '';
   }
 }
